733b49a1d8c756322f67e77fb5ac7b7b85d1d6b8
Tran Minh Son <kakalala2003@gmail.com>
2019-04-04 01:08:18 +0700

    Bo Sung Code CRUD_Law

--- Source Code/Chatbot20182/app/src/main/java/com/linearlayout/chatbot20182/MainActivity.java
+++ Source Code/Chatbot20182/app/src/main/java/com/linearlayout/chatbot20182/MainActivity.java
@@ -2,12 +2,62 @@ package com.linearlayout.chatbot20182;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.linearlayout.chatbot20182.Model.Law;
+import com.linearlayout.chatbot20182.data.DBManager;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText edtName;
+    private EditText edtDescription;
+    private Button btnSave;
+    private Button btnUpdate;
+    private ListView lvLaw;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final DBManager dbManager = new DBManager(this);
+
+        //Anh xa bien vao giao dien thong qua id
+
+        edtName = (EditText) findViewById(R.id.edt_name);
+        edtDescription = (EditText) findViewById(R.id.edt_description);
+        btnSave = (Button) findViewById(R.id.btn_save);
+        btnUpdate = (Button) findViewById(R.id.btn_update);
+
+        lvLaw = (ListView) findViewById(R.id.lv_law);
+
+        //Bat su kien cua nut Button
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 Law law =createLaw();
+                 if (law !=null)
+                 {
+                     dbManager.addLaw(law);
+                 }
+            }
+        });
     }
-}
+        private Law createLaw()
+    {
+        String name = edtName.getText().toString();
+        String description =edtDescription.getText().toString();
+
+        Law law =new Law(name,description);
+        return law;
+
+
+    }
+
+    }
+
--- Source Code/Chatbot20182/app/src/main/java/com/linearlayout/chatbot20182/data/DBManager.java
+++ Source Code/Chatbot20182/app/src/main/java/com/linearlayout/chatbot20182/data/DBManager.java
@@ -4,11 +4,14 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.nfc.Tag;
+import android.util.Log;
 
 import com.linearlayout.chatbot20182.Model.Law;
 
 public class DBManager extends SQLiteOpenHelper
 {
+    private final String Tag = "DBManager";
     private static final String DATABASE_NAME ="law";
     private static final String TABLE_NAME="lawlist";
     private static final String ID = "id";
@@ -28,18 +31,20 @@ public class DBManager extends SQLiteOpenHelper
     public DBManager(Context context) {
         super(context, DATABASE_NAME, null, VERSION);
         this.context=context;
+        Log.d(Tag,"DB Manager :");
     }
 
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(sqlQuery);
+        Log.d(Tag,"OnCreate");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
-
+        Log.d(Tag,"OnUpgrade");
 
 
     }
@@ -52,7 +57,8 @@ public class DBManager extends SQLiteOpenHelper
         values.put(DESCRIPTION,law.getmDescription());
 
         db.insert(TABLE_NAME,null,values);
-
+        db.close();
+        Log.d(Tag,"Add Law Sucessfully");
     }
 
 
