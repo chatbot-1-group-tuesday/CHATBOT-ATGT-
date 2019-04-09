4ec7044b36d99cdc9b26635e4fd8be41e065996e
Dinh ba Chien 20168630 <chien.db168630@sis.hust.edu.vn>
2019-04-09 19:37:42 +0700

    chien nop bai

--- Source Code/Chatbot20182/app/src/main/java/com/linearlayout/chatbot20182/MainActivity.java
+++ Source Code/Chatbot20182/app/src/main/java/com/linearlayout/chatbot20182/MainActivity.java
@@ -1,7 +1,16 @@
 package com.linearlayout.chatbot20182;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.FacebookSdk;
+import com.facebook.FacebookSdk;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+
     }
+
 }
+
