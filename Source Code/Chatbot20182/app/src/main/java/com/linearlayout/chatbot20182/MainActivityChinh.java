0633f20603dad9b9bb48ad31d48cf345eba34837
Tran Minh Son <kakalala2003@gmail.com>
2019-05-10 07:43:55 +0700

    Thêm chuc nang Đăng xuât cho ung dung Tran Minh Son

--- Source Code/Chatbot20182/app/src/main/java/com/linearlayout/chatbot20182/MainActivityChinh.java
+++ Source Code/Chatbot20182/app/src/main/java/com/linearlayout/chatbot20182/MainActivityChinh.java
@@ -1,7 +1,9 @@
 package com.linearlayout.chatbot20182;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +19,7 @@ public class MainActivityChinh extends AppCompatActivity
     private Button btnSign;
     private Button btnChat;
     private Button btnThongtin;
+    private Button btnDangXuat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,7 @@ public class MainActivityChinh extends AppCompatActivity
         setContentView(R.layout.activity_main_chinh);
         final DBManager dbManager = new DBManager(this);
         initWiget();
+        ControlButton();
 
         btnLaw.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,12 +61,39 @@ public class MainActivityChinh extends AppCompatActivity
             }
         });
     }
+// Phần Code thêm chức năng Đăng Xuất cho hệ thống
+    private void ControlButton() {
+        btnDangXuat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivityChinh.this,android.R.style.Theme_DeviceDefault_Light_Dialog);
+                builder.setTitle("Bạn có chắc muốn thoát khỏi ChatBot không ?");
+                builder.setMessage("Bạn hãy lựa chọn bên dưới để xác nhận ! ");
+                builder.setIcon(android.R.drawable.ic_dialog_alert);
+                builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        onBackPressed();
+                    }
+                });
+                builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                builder.show();
+            }
+        });
+    }
+
     private void initWiget() {
 
         btnLaw = (Button) findViewById(R.id.btn_law);
         btnSign = (Button) findViewById(R.id.btn_sign);
         btnChat=  (Button) findViewById(R.id.btn_chat);
         btnThongtin=(Button) findViewById(R.id.btn_thongtin);
+        btnDangXuat=(Button) findViewById(R.id.btn_dangxuat);
 
     }
 
